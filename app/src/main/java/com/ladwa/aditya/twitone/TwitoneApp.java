fbb7509e448004fa3fe8eefb19c181ae7ad6dda9
Aditya <ladwa.aditya@gmail.com>
2016-06-26 11:44:48 +0530

    Fect :Removed Retrolambda added MultiDex Support
    //TODO Added Twitter logic to connect to Twitterfe via Oauth

--- app/src/main/java/com/ladwa/aditya/twitone/TwitoneApp.java
+++ app/src/main/java/com/ladwa/aditya/twitone/TwitoneApp.java
@@ -1,11 +1,11 @@
 package com.ladwa.aditya.twitone;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by Aditya on 25-Jun-16.
  */
-public class TwitoneApp extends Application {
+public class TwitoneApp extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
