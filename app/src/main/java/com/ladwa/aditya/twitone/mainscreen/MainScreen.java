2b99922918513b12c62d96372c12c919cd28847c
Aditya <ladwa.aditya@gmail.com>
2016-06-27 14:20:27 +0530

    Fect : Addded MainScreen Contract and Presenter

--- app/src/main/java/com/ladwa/aditya/twitone/mainscreen/MainScreen.java
+++ app/src/main/java/com/ladwa/aditya/twitone/mainscreen/MainScreen.java
@@ -1,10 +1,12 @@
 package com.ladwa.aditya.twitone.mainscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.ladwa.aditya.twitone.R;
+import com.ladwa.aditya.twitone.login.LoginActivity;
 
 
 public class MainScreen extends AppCompatActivity {
@@ -15,6 +17,7 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        startActivity(new Intent(this, LoginActivity.class));
 
     }
 
---
+++ app/src/main/java/com/ladwa/aditya/twitone/mainscreen/MainScreenContract.java
@@ -0,0 +1,7 @@
+package com.ladwa.aditya.twitone.mainscreen;
+
+/**
+ * Created by Aditya on 27-Jun-16.
+ */
+public class MainScreenContract {
+}
