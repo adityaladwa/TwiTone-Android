fe735ce29ad876a57223643d9ff4e2b0906708b3
Aditya <ladwa.aditya@gmail.com>
2016-07-06 23:15:32 +0530

    Fect : Added Provider Matcher

--- app/src/main/java/com/ladwa/aditya/twitone/data/local/TwitoneProvider.java
+++ app/src/main/java/com/ladwa/aditya/twitone/data/local/TwitoneProvider.java
@@ -2,14 +2,31 @@ package com.ladwa.aditya.twitone.data.local;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+
+import java.util.regex.Matcher;
 
 /**
  * Created by Aditya on 06-Jul-16.
  */
 public class TwitoneProvider extends ContentProvider {
+
+    private static final int USER_ITEM = 100;
+
+    private static final UriMatcher sURI_MATCHER = buildUriMatcher();
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = TwitterContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority,TwitterContract.PATH_USER+ "/#" ,USER_ITEM);
+        return matcher;
+    }
+
+    private TwitterDbHelper mTwitterDbHelper;
 
     @Override
     public boolean onCreate() {
