3c9a3d8f6b12d67136fdda8c2033ca1a0e8b912b
Aditya <ladwa.aditya@gmail.com>
2016-07-13 15:03:14 +0530

    fect: Added Date Difference function

--- app/src/main/java/com/ladwa/aditya/twitone/util/Utility.java
+++ app/src/main/java/com/ladwa/aditya/twitone/util/Utility.java
@@ -1,17 +1,40 @@
 package com.ladwa.aditya.twitone.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 /**
+ * A Class with utility functions
  * Created by Aditya on 13-Jul-16.
  */
 public class Utility {
+
+    public static final long TWO_DAY_IN_SECOND = 172800000;
+
     public static String getDateTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat(
                 "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
         Date date = new Date();
         return dateFormat.format(date);
+    }
+
+    public static long getTimeDifference(String dbDate) {
+        Date d1 = null, d2 = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        String now = getDateTime();
+
+        try {
+            d2 = dateFormat.parse(now);
+            d1 = dateFormat.parse(dbDate);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        assert d2 != null;
+        assert d1 != null;
+        return d2.getTime() - d1.getTime();
     }
 }
--- app/src/test/java/com/ladwa/aditya/twitone/ExampleUnitTest.java
+++ app/src/test/java/com/ladwa/aditya/twitone/ExampleUnitTest.java
@@ -1,8 +1,10 @@
 package com.ladwa.aditya.twitone;
 
+import com.ladwa.aditya.twitone.util.Utility;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -11,5 +13,11 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void checkDate() throws Exception {
+        long timeDifference = Utility.getTimeDifference("2016-07-13 14:57:16");
+        System.out.println(timeDifference);
     }
 }
