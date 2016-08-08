f71f81c66b14a8e31155a7340f9a4b0be2d71174
Aditya <ladwa.aditya@gmail.com>
2016-08-08 11:06:52 +0530

    Fect: Added keystore and Constants files

---
+++ app/src/main/java/com/ladwa/aditya/twitone/util/Constants.java
@@ -0,0 +1,11 @@
+package com.ladwa.aditya.twitone.util;
+
+/**
+ * A class to keep all the constants that are used by this project
+ * Created by Aditya on 26-Jun-16.
+ */
+public class Constants {
+    public static final String OAUTH_COMSUMER_KEY = "s01kRJpmpGr783CSY6pQCs2Nc";
+    public static final String OAUTH_CONSUMER_SECRET = "CdxuKvF3QKoT34wWoYkslvlX3B80UU4mpg2btZXG2byWNXd3P6";
+    public static final String OAUTH_CALLBACK_URL = "oauth://twitoneforandroid";
+}
